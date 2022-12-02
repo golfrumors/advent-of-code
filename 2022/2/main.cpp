bc2382a56062efa22350e4dfb93ce2bdd034a7c6
golfrumors <golfrumors@users.noreply.github.com>
2022-12-02 00:22:11 -0500

    Solution for 2022/2 in C++ Pt.1 Rank: 3279, Pt.2 Rank: 2749

--- 2022/2/main.cpp
+++ 2022/2/main.cpp
@@ -2,13 +2,92 @@
 #include <fstream>
 #include <vector>
 
-int main(){
-	ifstream file("test.txt");
-	string tmp;
+using namespace std;
 
-	while(file >> tmp){
+int main(){
+	ifstream file("input.txt");
+	vector<char> move, Sug;
+	char m, s;
+	int points;
+
+	//a = rock b = paper c = scissors
+	//x = rock y = paper c = scissors
+
+	while(file >> m >> s){
+		move.push_back(m);
+		Sug.push_back(s);
+	}
+
+	for(int i = 0; i < move.size(); i++){
+		if(move[i] == 'A' && Sug[i] == 'Y'){
+			points += 6;
+		}
+
+		if(move[i] == 'B' && Sug[i] == 'Z'){
+			points += 6;
+		}
+
+		if(move[i] == 'C' && Sug[i] == 'X'){
+			points += 6;
+		}
+
+		if(Sug[i] == 'X'){
+			points += 1;
+		} else if(Sug[i] == 'Y'){
+			points += 2;
+		} else {
+			points += 3;
+		}
+
+		if(move[i] == 'A' && Sug[i] == 'X' || move[i] == 'B' && Sug[i] == 'Y' || move[i] == 'C' && Sug[i] == 'Z') {
+			points += 3;
+		}
 
 	}
+
+	cout << "Total of points for part one: " << points;
+
+	points = 0;
+
+	for(int i = 0; i < move.size(); i++){
+		switch(move[i]){
+			case 'A':
+				if(Sug[i] == 'X'){
+					points += 3; 
+				} else if(Sug[i] == 'Y'){
+					points += 3;
+					points += 1;
+				} else if(Sug[i] == 'Z'){
+					points += 6;
+					points += 2;
+				}
+				break;
+			case 'B':
+				if(Sug[i] == 'X'){
+					points += 1;
+				} else if(Sug[i] == 'Y'){
+					points += 3;
+					points += 2;
+				} else if(Sug[i] == 'Z'){
+					points += 6;
+					points += 3;
+				}
+				break;
+			case 'C':
+				if(Sug[i] == 'X'){
+					points += 2;
+				} else if(Sug[i] == 'Y'){
+					points += 3;
+					points += 3;
+				} else if(Sug[i] == 'Z'){
+					points += 6;
+					points += 1;
+				}
+				break;
+		}
+	}
+
+	cout << "\nTotal of points for part two: " << points;
 
 	return 0;
 }
