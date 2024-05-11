cda5d7ec04c060fc7ebde4fa8a6993d29a8e1e93
golfrumors <30129070+golfrumors@users.noreply.github.com>
2024-05-11 17:40:07 -0400

    a

--- 2022/14/main.cpp
+++ 2022/14/main.cpp
@@ -9,93 +9,136 @@
 
 using namespace std;
 
-struct Map
-{
-    vector< vector<char> > map = vector< vector<char> >(1'000, vector<char>(1'000, '.'));
+struct Map {
+    std::vector<std::vector<char>> map = std::vector<std::vector<char>>(1'000, std::vector<char>(1'000, ' '));
+    int min_row = 1'000;
+    int max_row = -1;
+    int min_col = 1'000;
+    int max_col = -1;
 
-    int min_x = 1'000;
-    int max_x = -1;
-    int min_y = 1'000;
-    int max_y = -1;
-
-    void updateBox(int x, int y)
-    {
-        if(min_x > x) min_x = x;
-        if(max_x < x) max_x = x;
-        if(min_y > y) min_y = y;
-        if(max_y < y) max_y = y;
+    void update_bounding_box(int row, int col) {
+        if (min_row > row) min_row = row;
+        if (max_row < row) max_row = row;
+        if (min_col > col) min_col = col;
+        if (max_col < col) max_col = col;
     }
 
-    void addScan(const string& line)
-    {
-        stringstream ss(line);
-        int prev_y = -1, prev_x = -1;
-        int y = 0, x = 0;
-
+    void add_scan(const std::string& line) {
+        std::stringstream s(line);
+        int prev_col = -1, prev_row = -1;
+        int col = 0, row = 0;
         char delim;
-
-        while(ss >> y >> delim >> x)
-        {
-            if(prev_y == y)
-                for (int i : std::views::iota(min(prev_x, x), max(prev_x, x)+1))
-                {
-                    map[i][y] = '#';
-                    updateBox(i, y);
-                }
-
-            if(prev_x == x)
-                for(int i : std::views::iota(min(prev_y, y), max(prev_y, y)+1))
-                {
-                    map[x][i] = '#';
-                    updateBox(x, i);
-                }
-
-            prev_x = x;
-            prev_y = y;
-
-            ss >> delim >> delim;
+        while (s >> col >> delim >> row) {
+            if (prev_col == col)
+            for (int idx : std::views::iota(std::min(prev_row, row), std::max(prev_row, row)+1)) {
+                map[idx][col] = '#';
+                update_bounding_box(idx,col);
+            }
+            if (prev_row == row)
+            for (int idx : std::views::iota(std::min(prev_col, col), std::max(prev_col, col)+1)) {
+                map[row][idx] = '#';
+                update_bounding_box(row,idx);
+            }
+            prev_row = row;
+            prev_col = col;
+            s >> delim >> delim; // process '->'
         }
     }
 
-    bool dropSandVoid()
-    {
-        int y = 0;
-        int x = 500;
+    bool drop_sand_into_void() {
+        int row = 0;
+        int col = 500;
 
-        auto step = [&y, &x, this]()
-        {
-            if (y+1 > max_y) return false;
-            if (x < min_x) return false;
-            if (x > max_x) return false;
+        auto step = [&row, &col, this]() {
+            // detect when the grain will fall into the void
+            if (row+1 > max_row) return false;
+            if (col < min_col) return false;
+            if (col > max_col) return false;
 
-            if(map[y+1][x] == '.') {y++;}
-            else if (map[y+1][x-1] == '.') {y++; x--;}
-            else if (map[y+1][x+1] == '.') {y++; x++;}
-            else {map[y][x] = '0'; return false;}
+            // can go down
+            if (map[row+1][col] == ' ') { row++; }
+            // can go down-left
+            else if (map[row+1][col-1] == ' ') { row++; col--; }
+            // can go down-right
+            else if (map[row+1][col+1] == ' ') { row++; col++; }
+            // stuck
+            else {
+                map[row][col] = 'O';
+                return false;
+            }
 
+            return true;
         };
 
-        while(step());
-
-        return map[y][x] == '0';
-
+        // move until stuck or destined for void
+        while (step());
+        // return true if stuck
+        return map[row][col] == 'O';
     }
 
+    bool drop_sand_onto_floor() {
+        if (map[0][500] == 'O') return false;
+
+        int row = 0;
+        int col = 500;
+
+        auto step = [&row, &col, this]() {
+            if (row < 0 || row >= std::ssize(map) || col < 0 || col >= std::ssize(map[0]))
+                throw std::runtime_error("Out of bounds, needs bigger map.");
+
+            if (row+1 == max_row+2) {
+                map[row][col] = 'O';
+                return false;
+            } else if (map[row+1][col] == ' ') { row++; }
+            else if (map[row+1][col-1] == ' ') { row++; col--; }
+            else if (map[row+1][col+1] == ' ') { row++; col++; }
+            else {
+                map[row][col] = 'O';
+                return false;
+            }
+
+            return true;
+        };
+
+        while (step());
+        return map[row][col] == 'O';
+    }
 };
 
-int main()
-{
-    fstream file("./input.txt");
+int parse_and_run(std::string_view path) {
+    using namespace day14::sand;
+
+    std::fstream file(path);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open " << std::quoted(path) << "\n";
+        return 1;
+    }
 
     Map map;
-    string l;
-    while(getline(file, l))
-        map.addScan(l);
+    std::string line;
+    while (getline(file, line))
+        map.add_scan(line);
 
     Map floor_map = map;
 
     size_t cnt = 0;
-    while(map.dropSandVoid()) cnt++;
+    while (map.drop_sand_into_void()) cnt++;
 
-    cout << cnt;
+    size_t floor_cnt = 0;
+    while (floor_map.drop_sand_onto_floor()) floor_cnt++;
+
+
+    std::cout << "Only the first " << cnt << " grains of sand will be captured by reservoir.\n";
+    std::cout << "The sand will stop falling after " << floor_cnt << " grains.\n";
+
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        return 1;
+    } else if (argc == 2) {
+        return parse_and_run("./input.txt");
+    }
 }
